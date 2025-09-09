e2b05f88d98d08f04650df08e4a67e6fa7866764
DHARNISH <dharnishhere@gmail.com>
2025-09-09 21:11:40 +0530

    Added utils.js

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const { getBook } = require("./utils");
 
 app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.get("/search", async (req, res) => {
+	const { bookName } = req.query;
+	console.log(bookName);
+	const result = await getBook(bookName);
+	console.log(result);
+});
 
 app.get("/", (req, res) => {
 	res.render("home");
---
+++ utils.js
@@ -0,0 +1,8 @@
+const apiKey = "AIzaSyDgrtvm5FRJe-qKn6VH2PHa9yF5MLTBGEY";
+const axios = require("axios");
+
+module.exports.getBook = async function (name) {
+	config = { params: { q: `${name}`, key: apiKey } };
+	const res = await axios.get("https://www.googleapis.com/books/v1/volumes", config);
+	return res.data.items[0];
+};
